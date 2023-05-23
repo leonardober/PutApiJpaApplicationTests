82c73ed3a4213cc162698f67f05372ee11114ac2
Leon <leonardober@gmail.com>
2023-05-23 14:15:42 -0500

    TestPruebas

---
+++ PutApiJPA/src/main/java/com/example/PutApiJPA/PutApiJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PutApiJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PutApiJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PutApiJpaApplication.class, args);
+	}
+
+}
